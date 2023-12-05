135315dd732e15235bac96e119c172078d38f806
ThisIsntTheWay <msblauwal6@gmail.com>
2023-12-05 21:15:46 +0100

    Fix puzzle

--- Day2/part1/main.go
+++ Day2/part1/main.go
@@ -41,80 +41,37 @@ func main() {
 			return c == ';' || c == ','
 		}
 		parts := strings.FieldsFunc(culledString, delimiterSplitter)
-		//fmt.Println(parts)
 
 		// Parse parts
 		// ["<num> <color>", ...]
 		isGameValid := true
 		for _, part := range parts {
+			if !isGameValid {
+				continue
+			}
+
 			splitPart := strings.Split(strings.TrimSpace(part), " ")
 
 			color := splitPart[1]
 			quantity, err := strconv.Atoi(splitPart[0])
 			check(err)
 
+			//fmt.Printf("%s %s -> %s %d\n", splitPart[1], splitPart[0], color, quantity)
+
 			if color == "red" {
-				isGameValid = quantity > 12
+				isGameValid = quantity <= 12
 			} else if color == "green" {
-				isGameValid = quantity > 13
+				isGameValid = quantity <= 13
 			} else { // Blue
-				isGameValid = quantity > 14
+				isGameValid = quantity <= 14
 			}
 		}
-
-		fmt.Printf("%d [%t] - %s\n", gameID, isGameValid, culledString)
-
-		// Abort loop if winning conditions are not met, add to result otherwise
 
 		if isGameValid {
-			result = result + gameID
+			result += gameID
 		}
-
-		/*
-			// Parse each set of the games
-			sets := strings.Split(culledString, ";")
-
-			var cubeSet [3]int
-			gameIsValid := true
-
-			for _, set := range sets {
-				pairs := strings.Split(strings.TrimSpace(set), ",")
-
-				for _, pair := range pairs {
-					var number int
-					var color string
-					_, err := fmt.Sscanf(pair, "%d %s", &number, &color)
-					if err != nil {
-						fmt.Printf("Error parsing pair: %s\n", pair)
-						continue
-					}
-
-					var cubeIndex int
-					if color == "red" {
-						cubeIndex = 0
-					} else if color == "green" {
-						cubeIndex = 1
-					} else {
-						cubeIndex = 2
-					}
-
-					cubeSet[cubeIndex] = cubeSet[cubeIndex] + number
-				}
-
-				// Pair verdict
-				for i := 0; i < len(cubeSet); i++ {
-					if cubeSet[i] > (12 + i) {
-						gameIsValid = false
-					}
-				}
-			}
-
-			if gameIsValid {
-				fmt.Printf("%d - %s\n", gameID, culledString)
-				result = result + gameID
-			}
-		*/
 	}
 
+	fmt.Println()
 	fmt.Printf("Result: %d", result)
 }
